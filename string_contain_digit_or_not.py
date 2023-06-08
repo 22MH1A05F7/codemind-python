ba1118b925d460ac3fdfb05b8511ba3791beeb3f
22MH1A05F7 <119723363+22MH1A05F7@users.noreply.github.com>
2023-06-08 14:54:25 +0530

    s=input() c=0 for i in s:     if i.isdigit():         c+=1 if c==0:     print("Doesn't contain digit") else:     print("Contains {} digit".format(c))

--- string_contain_digit_or_not.py
+++ string_contain_digit_or_not.py
@@ -1,10 +1,10 @@
 s=input()
 c=0
 for i in s:
-    if i.isdigit()==True:
+    if i.isdigit():
         c+=1
 if c==0:
     print("Doesn't contain digit")
 else:
-    print("Contains %d digit"%c)
+    print("Contains {} digit".format(c))
         
